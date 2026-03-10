da124355e2e2195fdb3f369882bd20ac58f17578
amitkumar02177-debug <amitkumar02177@gmail.com>
2026-03-10 22:32:03 +0530

    Add files via upload

---
+++ stock_web1/scripts.js
@@ -0,0 +1,28 @@
+async function loadStocks() {
+
+const response = await fetch("https://YOUR-API-URL/stocks");
+
+const data = await response.json();
+
+const table = document.querySelector("#stockTable tbody");
+
+table.innerHTML = "";
+
+data.forEach(item => {
+
+let row = `
+<tr>
+<td>${item.name}</td>
+<td>${item.qty}</td>
+</tr>
+`;
+
+table.innerHTML += row;
+
+});
+
+}
+
+loadStocks();
+
+setInterval(loadStocks, 30000);
